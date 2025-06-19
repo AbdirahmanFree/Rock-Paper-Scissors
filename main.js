e24d1c3fefed6f1b70fc9a2b4757ed5112019af8
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-06-18 22:50:00 -0400

    Created playround() function it compares human and computer choice and updates the global score variables according to the rules of rock paper scissors example: if human chose rock and computer chose scissors then humanScore is updated and a message is logged

--- main.js
+++ main.js
@@ -17,8 +17,51 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let choice = prompt("Rock Paper or Scissors?");
-    return choice
+    return choice.toLowerCase()
 
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice){
+        humanScore+=1;
+        computerScore+=1;
+        console.log(`Tie! you both selected ${humanChoice}`);
+    }
+    else {
+        if (humanChoice == 'rock') {
+            if (computerChoice == 'paper'){
+                computerScore +=1;
+                console.log(`You Lose! paper beats rock`);
+            }
+            else {
+                humanScore+=1;
+                console.log(`You Win! rock beats scissors`)
+            }
+        }
+        if (humanChoice == 'paper') {
+            if (computerChoice == 'rock'){
+                humanScore +=1;
+                console.log(`You win! paper beats rock`);
+            }
+            else {
+                computerScore+=1;
+                console.log(`You Lose! scissors beats paper`)
+            }
+        }
+        if (humanChoice == 'scissors') {
+            if (computerChoice == 'rock'){
+                computerScore +=1;
+                console.log(`You Lose! rock beats scissors`);
+            }
+            else {
+                humanScore+=1;
+                console.log(`You Win! scissors beats paper`)
+            }
+        }
+        
+    }
+
+    
 }
 
 
